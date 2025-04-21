589edf5b7c0ea8729f60682c3042cd56964e2949
Masttf <1124677368@qq.com>
2025-04-21 15:05:40 +0800

    init

---
+++ community-admin/src/main/java/fun/masttf/communityAdminApplication.java
@@ -0,0 +1,10 @@
+package fun.masttf;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class communityAdminApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(communityAdminApplication.class, args);
+    }
+}
---
+++ community-web/src/main/java/fun/masttf/controller/TestController.java
@@ -0,0 +1,16 @@
+package fun.masttf.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @RequestMapping(value = "/test", method=RequestMethod.GET)
+    public String test(){
+        return "test";
+    }
+}
